acb0581945e55bd00ad85c0934e5eada5b7ab8ab
Nujud98 <ialjod1998@gmail.com>
2024-12-15 10:26:17 +0300

    Initial Commit

---
+++ Aoun/AounApp.swift
@@ -0,0 +1,17 @@
+//
+//  AounApp.swift
+//  Aoun
+//
+//  Created by Nujud Alotaibi on 14/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AounApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AounTests/AounTests.swift
@@ -0,0 +1,17 @@
+//
+//  AounTests.swift
+//  AounTests
+//
+//  Created by Nujud Alotaibi on 14/06/1446 AH.
+//
+
+import Testing
+@testable import Aoun
+
+struct AounTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
